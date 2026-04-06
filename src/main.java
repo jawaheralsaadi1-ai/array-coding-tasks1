93878ffd7bb370d3f417e667d2bceed2c9153f4a
jawah <jawaheralsaadi1@gmail.com>
2026-04-06 22:59:01 +0400

    solve task3: reverse3

--- src/main.java
+++ src/main.java
@@ -5,7 +5,12 @@ public class main {
       //task2
         int[] a = {1, 2, 3};
         int[] b = {7, 3};
-        System.out.println("Task 2: " + commonEnd(a, b)); // المتوقع: true [cite: 20]
+        System.out.println("Task 2: " + commonEnd(a, b));
+        //task3
+        int[] nums = {1, 2, 3};
+        int[] result = reverse3(nums);
+        System.out.println("Task 3: [" + result[0] + "," + result[1] + "," + result[2] + "]");
+
     }
     public static boolean firstLast6(int[] nums) {
         return (nums[0] == 6 || nums[nums.length - 1] == 6);
@@ -14,7 +19,9 @@ public class main {
     public static boolean commonEnd(int[] a, int [] b) {
         return (a[0] ==b[0 ] || a[a.length -1 ] == b[b.length -1]);
     }
-
+public static int [] reverse3(int[] nums){
+        return new int[] {nums[2], nums[1], nums[0]};
+}
 
 
     }
