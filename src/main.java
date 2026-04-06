2800a8038b159c6c8ef139af790728b50ba0c4fc
jawah <jawaheralsaadi1@gmail.com>
2026-04-06 22:08:37 +0400

    solve task2

--- src/main.java
+++ src/main.java
@@ -6,6 +6,11 @@ public class main {
     public static boolean firstLast6(int[] nums) {
         return (nums[0] == 6 || nums[nums.length - 1] == 6);
     }
+// task2
+    public static boolean firstLast6(int[] a, int [] b) {
+        return (a[0] ==b[0 ] || a[a.length -1 ] == b[b.length -1]);
+    }
 
+ }
 
 }
