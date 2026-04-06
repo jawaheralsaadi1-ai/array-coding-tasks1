9fabfe6db4b94be9827d7df1d4549eecc5de2b2d
jawah <jawaheralsaadi1@gmail.com>
2026-04-06 22:30:51 +0400

    Solve task2: commonEnd

--- src/main.java
+++ src/main.java
@@ -1,18 +1,20 @@
 public class main {
     public static void main (String[]args){
       int[] myArray = {1, 2, 6};
-      System.out.println(firstLast6(myArray));
+      System.out.println("Task 1: " + firstLast6(myArray));
+      //task2
+        int[] a = {1, 2, 3};
+        int[] b = {7, 3};
+        System.out.println("Task 2: " + commonEnd(a, b)); // المتوقع: true [cite: 20]
     }
     public static boolean firstLast6(int[] nums) {
         return (nums[0] == 6 || nums[nums.length - 1] == 6);
     }
 // task2
-    public static boolean firstLast6(int[] a, int [] b) {
+    public static boolean commonEnd(int[] a, int [] b) {
         return (a[0] ==b[0 ] || a[a.length -1 ] == b[b.length -1]);
     }
-    // task 3
-    public static int[] reverse3 (int[]nums) {
-        return new int[]{nums[2], nums[1], nums[0]};
 
-    }
+
+
     }
